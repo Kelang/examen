89ed94471a1d7a852bb8b2ab2106d08cd840b8aa
Kevin Langlois <k.langlois001@gmail.com>
2019-03-19 09:48:34 -0400

    inclusion des images dans les liens des nouvelles

--- template-parts/content/frontpage-quatre-images.php
+++ template-parts/content/frontpage-quatre-images.php
@@ -13,7 +13,7 @@
 <div class="wp-block-column">
 	<figure class="wp-block-image">
 	 
-
+	<a href="<?php the_permalink(); ?>"><?php the_title(); ?>
 	<?php 
 //	the_post_thumbnail(''); 
 
@@ -26,7 +26,7 @@
 	// add_post_meta($post_id, '_thumbnail_id', $attachment_id);
 	
 	?>
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</a>
 	</figcaption>
 	</figure>
 </div>
